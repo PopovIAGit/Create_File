92e9a7812ff5608ce8a8554be79376f9bf643c8f
PopovIA <myxxxwm@gmail.com>
2024-12-06 14:19:00 +0700

    начал добавлять парсер
    добавил енамы для стандарнтых строк
    добавил промежуточный парсер который возвращает заполнение строковых параметров в find_strings
    Для корректной работы файлы парамс и меню_прамс надо доработать строками

--- main.py
+++ main.py
@@ -2,17 +2,29 @@ import os
 import openpyxl
 
 from domain import check_app_version
+from enum import Enum
 
 #todo создание промежуточных строк вынести в функцию и вызывать при парсинге файла
 
+class ParamType(Enum):
+    UNION = "UNION"
+    UINT16 = "UINT16"
+    INT16 = "INT16"
+    STR = "STR"
+
+class ParamView(Enum):
+    DEC = "DEC"
+    HEX = "HEX"
+    BIN = "BIN"
+
 class Param:
     def __init__(self):
-        self.class_num = 0  # Класс числовой
+        self.code = 0  # Класс числовой
         self.name = ""  # название текстовое
         self.value = None  # значение пустое
         self.units = ""  # Единицы измерения текстовое
-        self.type = ""  # тип строковый
-        self.view = ""  # Вид строковый
+        self.type = ParamType.STR  # тип строковый
+        self.view = ParamView.DEC  # Вид строковый
         self.address = 0  # адрес числовой
         self.record = 0  # Запись числовой
         self.min_value = 0  # Минимум числовой
@@ -32,7 +44,11 @@ class Param:
         self.date_changed = ""  # Дата изменения текстовый
         self.author = ""  # Автор текстовый
 
-def create_exel_file():
+def create_exel_file(excel_file):
+    """
+    Создание файла Excel с 8 строками данных и указанными заголовками.
+    """
+    
     wb = openpyxl.Workbook()
     # Select the first sheet
     sheet = wb.active
@@ -81,7 +97,7 @@ def create_exel_file():
         ["3", "D Команды управления"],
         ["4", "G. Параметры теста"],
         ["5", "H. Скрытые параметры"],
-        ["6", "R. Технологический регистр"],
+        ["6", "T. Технологический регистр"],
         ["7", "E Журнал событий"]
     ]
     
@@ -94,30 +110,128 @@ def create_exel_file():
     for i, header in enumerate(headers):
         sheet.column_dimensions[openpyxl.utils.get_column_letter(i+1)].width = len(header) * 1.2
     
+    wb.save(excel_file)
 
-    wb.save("example.xlsx")
-
-def create_file(filename, text):
+def create_file(filename: str, text: str) -> None:
+    """
+    Создание файла с указанным именем и текстом.
+    
+    Args:
+        filename (str): Имя файла для создания.
+        text (str): Текст для записи в файл.
+    """
     with open(filename, "w", encoding="utf-8") as f:
         f.write(text)
 
-def find_files():
+def find_file():
+    """
+    Поиск файла 'params.h' или 'menu_params.h' в текущем каталоге и его
+    подкаталогах.
+
+    Returns:
+        str: Путь найденного файла или None, если файл не найден
+    """
     filenames = ["params.h", "menu_params.h"]
     for filename in filenames:
+        # Walk through the directory tree
         for root, dirs, files in os.walk("."):
+            # Check if the current filename is in the list of files
             if filename in files:
-                print(f"Found {filename} at {os.path.join(root, filename)}")
-                break
-        else:
-            print(f"Error: {filename} not found")
+                return os.path.join(root, filename)
 
+    # File not found in any directory
+    print("Error: Neither 'params.h' nor 'menu_params.h' found")
+    return None
+
+def find_strings(file_path, number):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        params_start = None
+        params_end = None
+        for i, line in enumerate(lines):
+            if line.strip() == "//! Строки":
+                params_start = i
+            elif line.strip() == "//! Строки конец":
+                params_end = i
+                break
+        if params_start is not None and params_end is not None:
+            lines = lines[params_start+1:params_end]
+            result = []
+            found = False
+            for line in lines:
+                if found:
+                    if line.strip() == "//!----------------":
+                        break
+                    parts = line.split('"')
+                    if len(parts) >= 3:
+                        result.append(parts[1].strip())
+                elif "//" in line and line.split("//")[1].strip() == str(number):
+                    found = True
+                    parts = line.split('"')
+                    if len(parts) >= 3:
+                        result.append(parts[1].strip())
+            return result
+
+
+def parse_file(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        params_start = None
+        params_end = None
+        for i, line in enumerate(lines):
+            if line.strip() == "//! Параметры":
+                params_start = i
+            elif line.strip() == "//! Параметры конец":
+                params_end = i
+                break
+        if params_start is not None and params_end is not None:
+            params_lines = lines[params_start+1:params_end]
+            # Create a dictionary to map indices to lines
+            index_lines = {}
+            for line in params_lines:
+                index = line.split('.')[0]
+                if index in index_lines:
+                    index_lines[index].append(line)
+                else:
+                    index_lines[index] = [line]
+            
 def main():
-    #create_file("test.txt", "привет мир")
-    #find_files()
-    #create_exel_file()
+    """
+    Главная функция программы.
+
+    Эта функция вызывается, когда программа запускается из
+    командной строки. Она содержит основную логику программы и
+    отвечает за выполнение задач, указанных в аргументах
+    командной строки.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+    # Create a text file with the specified text
+    #create_file("test.txt", " ")
+
+    # Search for specific files in the current directory and its subdirectories
+    file_path = find_file()
+    
+    strings = find_strings(file_path, 260)
+    
+    print(strings)
+    
+    # Create an name of the Excel file
+    #excel_file = "example.xlsx"
+    # Create an Excel file
+    #create_exel_file(excel_file)
+    # Parse the file
+    #parse_file(file_path, excel_file)
+
+    # Get the current version of the application
     version = check_app_version()
     print(version)
     
     
 if __name__ == "__main__":
     main()
+
